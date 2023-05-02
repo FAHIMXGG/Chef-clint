1118d5201f64bc13b481d16f20b783a2ac28522a
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-05-02 11:30:15 +0600

    reg page

--- src/page/login/reg/Reg.jsx
+++ src/page/login/reg/Reg.jsx
@@ -1,9 +1,103 @@
+import { Button, Checkbox, Label, TextInput } from 'flowbite-react';
 import React from 'react';
 
 const Reg = () => {
     return (
         <div>
-            <h1>reg</h1>
+            <form className="flex flex-col gap-4">
+                <div>
+                    <div className="mb-2 block">
+                        <Label
+                            htmlFor="name"
+                            value="Your Name"
+                        />
+                    </div>
+                    <TextInput
+                        id="email2"
+                        type="text"
+                        name='name'
+                        placeholder="Name"
+                        required={true}
+                        shadow={true}
+                    />
+                </div>
+                <div>
+                    <div className="mb-2 block">
+                        <Label
+                            htmlFor="photo"
+                            value="Your Photo"
+                        />
+                    </div>
+                    <TextInput
+                        id="email2"
+                        type="text"
+                        name='photo'
+                        placeholder="Photo Url"
+                        required={true}
+                        shadow={true}
+                    />
+                </div>
+                <div>
+                    <div className="mb-2 block">
+                        <Label
+                            htmlFor="email2"
+                            value="Your email"
+                        />
+                    </div>
+                    <TextInput
+                        id="email2"
+                        type="email"
+                        name='email'
+                        placeholder="name@flowbite.com"
+                        required={true}
+                        shadow={true}
+                    />
+                </div>
+                <div>
+                    <div className="mb-2 block">
+                        <Label
+                            htmlFor="password2"
+                            value="Your password"
+                        />
+                    </div>
+                    <TextInput
+                        id="password2"
+                        type="password"
+                        name='password'
+                        required={true}
+                        shadow={true}
+                    />
+                </div>
+                <div>
+                    <div className="mb-2 block">
+                        <Label
+                            htmlFor="repeat-password"
+                            value="Repeat password"
+                        />
+                    </div>
+                    <TextInput
+                        id="repeat-password"
+                        type="password"
+                        required={true}
+                        shadow={true}
+                    />
+                </div>
+                <div className="flex items-center gap-2">
+                    <Checkbox id="agree" />
+                    <Label htmlFor="agree">
+                        I agree with the
+                        <a
+                            href="/forms"
+                            className="text-blue-600 hover:underline dark:text-blue-500"
+                        >
+                            terms and conditions
+                        </a>
+                    </Label>
+                </div>
+                <Button type="submit">
+                    Register new account
+                </Button>
+            </form>
         </div>
     );
 };
